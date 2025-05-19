c34549ce96a161ff7ecac307f72d0c899e052f95
Alexandru Dragos <61319009+GAlexandruD@users.noreply.github.com>
2025-05-19 18:53:11 +0300

    Create update.sh

---
+++ update-script/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+
+# Check for new version
+NEXT_VERSION=$(npm show next version)
+CURRENT_VERSION=$(<../nextjs-version.txt)
+
+if [[ "$NEXT_VERSION" != "$CURRENT_VERSION" ]]; then
+  echo "New version available: $NEXT_VERSION (current is $CURRENT_VERSION)"
+  
+  # Removes the content of the nextjs-files folder.
+  rm -rf ../nextjs-files/*
+
+  # Uses npx to create next app with default settings.
+  npx -y create-next-app@latest ../nextjs-files --yes
+
+  # Update version in nextjs-version.txt
+  echo "$NEXT_VERSION" > nextjs-version.txt
+
+  # Adds the files to the repository
+  git add .
+  git commit -m "Update to Next.js v$NEXT_VERSION"
+  git push
+
+else
+  echo "Already up to date: $CURRENT_VERSION"
+fi
